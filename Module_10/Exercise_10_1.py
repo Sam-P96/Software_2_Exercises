3525ba06ef4d2bb7b49018e636ab65c83375c0b5
Samnap Peo <Samnappeo@gmail.com>
2025-10-29 20:41:07 +0200

    10.1 done

--- Module_10/Exercise_10_1.py
+++ Module_10/Exercise_10_1.py
@@ -2,7 +2,39 @@ class Elevator:
     def __init__(self, bottom_f, top_f):
         self.bottom_f = bottom_f
         self.top_f = top_f
-        self.position = 0
+        self.position = bottom_f
 
     def floor_up(self):
+        print(f"Going up. Current Position: {self.position}")
+        self.position += 1
+
+    def floor_down(self):
+        print(f"Going down. Current Position: {self.position}")
+        self.position -= 1
+
+    def go_to_floor(self, goal):
+        if self.bottom_f <= goal <= self.top_f:
+            while self.position != goal:
+                if self.position > goal:
+                    self.floor_down()
+                elif self.position < goal:
+                    self.floor_up()
+                else:
+                    #Just for a quick debug
+                    print("This shouldn't be happening, check the code")
+                    break
+            print(f"Floor reached. Current Position: {self.position}")
+        else:
+            print(f"Invalid floor, the elevator cannot reach floor {goal}")
+
+
+h = Elevator(3, 7)
+h.go_to_floor(6)
+h.go_to_floor(4)
+h.go_to_floor(7)
+h.go_to_floor(1)
+h.go_to_floor(3)
+
+
+
 
